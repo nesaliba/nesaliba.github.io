7a9bfa1477676136a1a9a4f96328e819e88f22da
Victor Wu <victorlwu0512@gmail.com>
2026-03-19 15:04:22 -0600

    Attempt to fix classic games laoding issue.

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ function getHtmlFiles(dir, fileList = []) {
   for (const file of files) {
     const filePath = resolve(dir, file);
     if (fs.statSync(filePath).isDirectory()) {
-      // Added 'public' to the ignore list so Vite doesn't crash on Unity games
-      if (file !== 'node_modules' && file !== '.git' && file !== 'dist' && file !== 'public') {
+      // Added 'legacy-games' to the ignore list so Vite doesn't crash
+      if (file !== 'node_modules' && file !== '.git' && file !== 'dist' && file !== 'legacy-games') {
         getHtmlFiles(filePath, fileList);
       }
     } else if (filePath.endsWith('.html')) {
@@ -22,7 +22,6 @@ function getHtmlFiles(dir, fileList = []) {
 const htmlFiles = getHtmlFiles(resolve(__dirname));
 const input = {};
 
-// Create an entry point dictionary for Vite's bundler
 htmlFiles.forEach((file) => {
   const name = file
     .replace(__dirname, '')
@@ -34,7 +33,6 @@ htmlFiles.forEach((file) => {
 });
 
 export default defineConfig({
-  // Base URL for GitHub Pages
   base: '/', 
   resolve: {
     alias: {
